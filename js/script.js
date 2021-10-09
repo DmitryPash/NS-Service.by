6e363eb1797a711aeba212f92f36ab8877880af8
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2021-10-09 15:05:38 +0100

    Add css style
    add dropbox menu/change style of catalog

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-let inputBtn = document.querySelectorAll('.item-add-button > .input')
+let inputBtn = document.querySelectorAll(".item-add-button > .input");
 // inputBtn.value= 0;
 
-const minus = () =>{
-    inputBtn[0].value > 0 ? inputBtn[0].value-- : false;
-}
-const plus = () =>{
-    inputBtn[0].value++
-}
+const minus = () => {
+  inputBtn[0].value > 0 ? inputBtn[0].value-- : false;
+};
+const plus = () => {
+  inputBtn[0].value++;
+};
 let reg = /[A-Za-za-Яа-яËë]/g;
 
-const changeHandler = e => {
-    const value = e.value
-    e.value = value.replace(reg, '')
-}
+const changeHandler = (e) => {
+  const value = e.value;
+  e.value = value.replace(reg, "");
+};
 
+$(".dropdown").click(function () {
+  $(".dropdown-menu").toggleClass("show");
+});
