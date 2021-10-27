94483efb7dd96faa097850aa124b6290bc44906f
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2021-10-27 11:10:02 +0100

    tabs
    tabs

--- js/script.js
+++ js/script.js
@@ -31,5 +31,37 @@ inputBtns.forEach((element) => {
   plusBtn.addEventListener('click', plus(inputForm))
 });
 
+              
+              $.fn.Tabs = function() {
+                var selector = this;
+
+                this.each(function() {
+                   var obj = $(this); 
+                   $(obj.attr('href')).hide();
+                   $(obj).click(function() {
+                      $(selector).removeClass('selected');
+                      
+                      $(selector).each(function(i, element) {
+                         $($(element).attr('href')).hide();
+                      });
+                      
+                      $(this).addClass('selected');
+                      $($(this).attr('href')).fadeIn();
+                      return false;
+                   });
+                });
+
+                $(this).show();
+                $(this).first().click();
+                if(location.hash!='' && $('a[href="' + location.hash + '"]').length)
+                   $('a[href="' + location.hash + '"]').click();	
+             };
+
+
+             jQuery(document).ready(function($){
+
+                $('.recommend-tabs a').Tabs();
+
+             });
 
 
